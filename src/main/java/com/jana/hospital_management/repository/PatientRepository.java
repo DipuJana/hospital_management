3ebaeb0d7c146fbdd073376748df914ad1c3f8b1
Dipanjan Jana <dipanjanjana001@gmail.com>
2026-03-29 23:00:51 +0530

    refactor(repository): add JpaSpecificationExecutor to support dynamic filtering

--- src/main/java/com/jana/hospital_management/repository/PatientRepository.java
+++ src/main/java/com/jana/hospital_management/repository/PatientRepository.java
@@ -2,10 +2,13 @@ package com.jana.hospital_management.repository;
 
 import com.jana.hospital_management.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface PatientRepository extends JpaRepository<Patient, Long> {
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long>, JpaSpecificationExecutor<Patient> {
 
     boolean existsByEmail(String email);
     Optional<Patient> findByEmail(String email);
